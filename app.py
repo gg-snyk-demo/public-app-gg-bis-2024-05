b5c200bc9457fa6bd2bf756f3eedfc725f775684
Tony Stark <tony-stark-snyk@protonmail.com>
2024-06-14 10:03:46 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import antigravity
 
-client_id=3MVG9_dg1sJ97QZWri__jd300xpW.0f7gkPO0l19CCOjKv2ITCCG6Sg7z5c5ULpb1m2XXf.vHpCbCxd_lQcjw
-client_secret=3774840646649758818
+SEED=5f36e7a7-7bdc-4f86-a913-abee72b13ac7
 
 def main():
     antigravity.fly()
